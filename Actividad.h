677c15c565738c1f14a692cfa5f732bc37bdf09b
JessacN4v4 <a01713439@tec.mx>
2025-10-24 15:45:24 -0500

    Actividad.h
    Reescripcion de la clase actividad.h

---
+++ Actividad.h
@@ -0,0 +1,22 @@
+#ifndef ACTIVIDAD_H
+#define ACTIVIDAD_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Actividad {
+public:
+    string nombre;
+    int prioridad;
+    bool completada;
+
+    Actividad(string n = "", int p = 0) : nombre(n), prioridad(p), completada(false) {}
+
+    void mostrar() {
+        cout << nombre << " | Prioridad: " << prioridad
+             << " | Estado: " << (completada ? "Completada" : "Pendiente") << endl;
+    }
+};
+
+#endif
