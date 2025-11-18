c1365ec014ef07fc8fa011694caa8ee04b6f6f6c
JessacN4v4 <a01713439@tec.mx>
2025-11-18 11:14:11 -0600

    Update gestor.h av1

--- gestor.h
+++ gestor.h
@@ -1,94 +1,61 @@
 #ifndef GESTOR_H
 #define GESTOR_H
 
-#include <iostream>
 #include <vector>
+#include <iostream>
 #include "actividad.h"
+#include "sorts.h"
 using namespace std;
 
-class Gestor {
+class Gestor_actividades {
 private:
-    vector<Actividad> lista;
-
-    void merge(int left, int mid, int right) {
-        vector<Actividad> temp;
-        int i = left, j = mid + 1;
-        while (i <= mid && j <= right) {
-            if (lista[i].prioridad >= lista[j].prioridad)
-                temp.push_back(lista[i++]);
-            else
-                temp.push_back(lista[j++]);
-        }
-        while (i <= mid) temp.push_back(lista[i++]);
-        while (j <= right) temp.push_back(lista[j++]);
-        for (int k = left; k <= right; k++) lista[k] = temp[k - left];
-    }
-
-    void merge_sort(int left, int right) {
-        if (left >= right) return;
-        int mid = left + (right - left) / 2;
-        merge_sort(left, mid);
-        merge_sort(mid + 1, right);
-        merge(left, mid, right);
-    }
+    vector<Actividad> actividades;
 
 public:
-    void agregar(Actividad a) {
-        lista.push_back(a);
-        cout << "Actividad agregada.\n";
+    void agregar_actividad(const Actividad& act) {
+        actividades.push_back(act);
+        cout << "Actividad agregada exitosamente." << endl;
     }
-
-    void mostrar() {
-        if (lista.empty()) {
-            cout << "No hay actividades.\n";
+    
+    void mostrar_actividades() const {
+        if (actividades.empty()) {
+            cout << "No hay actividades registradas." << endl;
             return;
         }
-        cout << "\nLista de actividades:\n";
-        for (int i = 0; i < lista.size(); i++) {
-            cout << i + 1 << ". ";
-            lista[i].mostrar();
+        
+        cout << "\n LISTA DE ACTIVIDADES \n" << endl;
+        for (size_t i = 0; i < actividades.size(); i++) {
+            cout << "Actividad #" << (i + 1) << ":" << endl;
+            actividades[i].mostrar();
         }
     }
-
-    void marcar(int i) {
-        if (i < 1 || i > lista.size()) return;
-        lista[i - 1].completada = true;
-        cout << "Actividad marcada como completada.\n";
-    }
-
-    void eliminar(int i) {
-        if (i < 1 || i > lista.size()) return;
-        lista.erase(lista.begin() + i - 1);
-        cout << "Actividad eliminada.\n";
-    }
-
-    void bubble_sort() {
-        for (int i = 0; i < lista.size() - 1; i++) {
-            for (int j = 0; j < lista.size() - i - 1; j++) {
-                if (lista[j].prioridad < lista[j + 1].prioridad)
-                    swap(lista[j], lista[j + 1]);
-            }
+    
+    void marcar_completada(int indice) {
+        if (indice < 0 || indice >= (int)actividades.size()) {
+            cout << "Índice inválido." << endl;
+            return;
         }
-        cout << "Ordenado por prioridad (Bubble Sort).\n";
+        
+        actividades[indice].marcar_completada();
+        cout << "Actividad marcada como completada." << endl;
     }
-
-    void selection_sort() {
-        for (int i = 0; i < lista.size() - 1; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < lista.size(); j++) {
-                if (lista[j].nombre < lista[minIndex].nombre)
-                    minIndex = j;
-            }
-            swap(lista[i], lista[minIndex]);
+    
+    void eliminar_actividad(int indice) {
+        if (indice < 0 || indice >= (int)actividades.size()) {
+            cout << "Índice inválido." << endl;
+            return;
         }
-        cout << "Ordenado alfabéticamente (Selection Sort).\n";
+        
+        actividades.erase(actividades.begin() + indice);
+        cout << "Actividad eliminada exitosamente." << endl;
     }
-
-    void ordenar_merge() {
-        if (!lista.empty()) {
-            merge_sort(0, lista.size() - 1);
-            cout << "Ordenado por prioridad (Merge Sort).\n";
-        }
+    
+    void ordenar_prioridad() {
+        Sorts::ordenar_automatico(actividades);
+    }
+    
+    int get_cantidad_actividades() const {
+        return actividades.size();
     }
 };
 
